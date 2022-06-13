db39445adeaaf0772df66d1fd895df5d7e479112
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-13 13:50:35 +0200

    deleted line

--- scripts/bcli-backup.js
+++ scripts/bcli-backup.js
@@ -5,7 +5,6 @@ console.log('backing up .env file...');
 
 const targetFilename = 'D:\\general-backup\\' + qdat.timeStampifyFileName('.env');
 
-
 copyFile('.env', targetFilename, (err) => {
 	if (err)
 		throw err;
