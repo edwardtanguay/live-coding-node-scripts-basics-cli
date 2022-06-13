ff2d24e454ee8b13ea271da81436a1b307bb65b0
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-13 11:56:49 +0200

    finished

--- scripts/bcli-backup.js
+++ scripts/bcli-backup.js
@@ -5,6 +5,7 @@ console.log('backing up .env file...');
 
 const targetFilename = 'D:\\general-backup\\' + qdat.timeStampifyFileName('.env');
 
+
 copyFile('.env', targetFilename, (err) => {
 	if (err)
 		throw err;
--- scripts/qtools/qdat.js
+++ scripts/qtools/qdat.js
@@ -1,3 +1,10 @@
+/**
+* Returns text string containing current date-and-time-stamp, used for labeling files, etc.
+*
+* qdat.getCurrentDateTime()
+*
+* 2022-06-13-06-23-19
+*/
 export const getCurrentDateTime = () => {
     var now = new Date();
     var year = now.getFullYear();
@@ -24,7 +31,18 @@ export const getCurrentDateTime = () => {
     var dateTime = year + '-' + month + '-' + day + '-' + hour + '-' + minute + '-' + second;
     return dateTime;
 }
- 
+
+/**
+* Adds a suffix to the end of a filename so that it has a timestamp, used for backup or log files, etc.
+*
+* qdat.tempStampifyFileName('index.html')
+*
+* index.html.backup-2022-06-13-06-23-19
+*
+* qdat.tempStampifyFileName('index.html','log')
+*
+* index.html.log-2022-06-13-06-23-19
+*/
 export const timeStampifyFileName = (filename, idCode='backup') => {
     return `${filename}.${idCode}-${getCurrentDateTime()}`;
 }
